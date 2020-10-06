aa7005af3cc3649578c8989adb9b9fc004be9318
Darren Tebo <darrentebo83@outlook.com>
2020-10-06 14:24:59 -0400

    connected the app to the redux store

---
+++ src/actions/CarActions.js
@@ -0,0 +1,10 @@
+import AddedFeature from "../components/AddedFeature";
+
+export const ADD_FEATURE = 'ADD_FEATURE';
+
+export const addFeature = feature => {
+    return {
+        type: ADD_FEATURE,
+        payload: feature
+    };
+};
